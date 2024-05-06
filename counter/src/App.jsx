01842f7a2b4d9a8a8f996e85fabebf207c0e5391
chaithanyachandrachud <chaithanya.chud@gmail.com>
2024-05-06 12:29:04 +0530

    first commit

---
+++ counter/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Component from "./Components/Component";
+
+function App() {
+  return (
+    <div>
+      <Component>
+      </Component>
+      
+    </div>
+  )
+}
+
+export default App;
